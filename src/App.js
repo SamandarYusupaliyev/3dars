e99d1900bec1a318c03985b1f7ebc3523cb70a23
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-03-28 16:07:46 +0500

    papka

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleAddTask = () => {
+    if (newTask) {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
+  const handleRemoveTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Create your ToDo list</h1>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        placeholder="Enter a new task"
+        className="input__task"
+      />
+      <button onClick={handleAddTask} className="btn__task">
+        Add Task
+      </button>
+      <ul className="list__task">
+        {tasks.map((task, index) => (
+          <li key={index} className="item__task">
+            {task}
+            <button
+              onClick={() => handleRemoveTask(index)}
+              className="btn__remove"
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div className="section__main">
+        {/* <li className="item">Get IELTS</li> */}
+      </div>
+      <div className="footer">
+        <p className="text__footer">Created by </p>
+        <p className="text__footer">Yusupaliyev Samandar</p>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default ToDoList;
